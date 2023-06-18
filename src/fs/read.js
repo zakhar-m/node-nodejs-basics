eaf8d5b677e932a1ba17ae3b83fbb50f361e9c9f
Zakhar M <zakharm.rs@gmail.com>
2023-06-18 17:04:51 +0200

    feat: implement read functionality

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import { readFile } from "node:fs";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = "./src/fs/files/fileToRead.txt";
+
+  readFile(filePath, "utf8", (err, data) => {
+    if (err) throw new Error("FS operation failed");
+
+    console.log(data);
+  });
 };
 
 await read();
