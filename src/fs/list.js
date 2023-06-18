5895643d8cadb4e0740ef5d7eaa462f9b2bf59ae
Zakhar M <zakharm.rs@gmail.com>
2023-06-18 16:54:56 +0200

    feat: implement list functionality

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import { access, readdir } from "node:fs";
+
 const list = async () => {
-    // Write your code here 
+  const dirPath = "./src/fs/files/";
+
+  access(dirPath, (err) => {
+    if (err) throw new Error("FS operation failed");
+
+    readdir(dirPath, (err, files) => {
+      if (err) throw err;
+
+      files.forEach((fileName) => console.log(fileName));
+    });
+  });
 };
 
 await list();
