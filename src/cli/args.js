d62f64a8a4f014c0226774da437293e4c5fcea70
Zakhar M <zakharm.rs@gmail.com>
2023-06-18 18:19:11 +0200

    feat: implement args functionality

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const argValuePairs = [];
+
+  while (args.length > 0) argValuePairs.push(args.splice(0, 2));
+
+  const result = argValuePairs
+    .map(([arg, value]) => `${arg.slice(2)} is ${value}`)
+    .join(", ");
+  console.log(result);
 };
 
 parseArgs();
