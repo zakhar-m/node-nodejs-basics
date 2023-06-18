124dc7b9f5b95044745f2779e0913beabe33fc0f
Zakhar M <zakharm.rs@gmail.com>
2023-06-18 15:51:39 +0200

    feat: implement copy functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import { cp, access } from "node:fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  const fromPath = "./src/fs/files/";
+  const toPath = "./src/fs/files_copy/";
+
+  async function dirExists(path) {
+    try {
+      await access(path);
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  try {
+    const fromDirExists = await dirExists(fromPath);
+    const toDirExists = await dirExists(toPath);
+
+    if (!fromDirExists || toDirExists) {
+      throw new Error("FS operation failed");
+    }
+
+    await cp(fromPath, toPath, { recursive: true });
+  } catch (err) {
+    throw err;
+  }
 };
 
 await copy();
