2e8f0701cb812eedfa9aa2ca2ac751d4dc5f5131
Zakhar M <zakharm.rs@gmail.com>
2023-06-19 22:05:26 +0200

    feat: implement transform stream functionality

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform } from "node:stream";
+
 const transform = async () => {
-    // Write your code here 
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const transformedData = [...chunk.toString()].reverse().join("");
+
+      callback(null, transformedData);
+    },
+  });
+
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
