887520d5b29c08326da91dabd005d420efbec344
Zakhar M <zakharm.rs@gmail.com>
2023-06-19 23:18:00 +0200

    feat: implement compress/decompress functionality

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import {createWriteStream, createReadStream} from 'node:fs';
+import {createGzip} from 'node:zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const filePath = './src/zip/files/fileToCompress.txt';
+    const archivePath = './src/zip/files/archive.gz';
+
+    const rStream = createReadStream(filePath);
+    const wStream = createWriteStream(archivePath);
+
+    rStream.pipe(createGzip()).pipe(wStream);
 };
 
 await compress();
